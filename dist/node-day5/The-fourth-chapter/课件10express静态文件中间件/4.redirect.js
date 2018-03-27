28cbdd527ef73a0ca07e8f33a80f92abed1536ce
KLNL100 <1343537756@qq.com>
2018-03-27 14:46:33 +0800

    0327_3

--- dist/node-day5/The-fourth-chapter/课件10express静态文件中间件/4.redirect.js
+++ dist/node-day5/The-fourth-chapter/课件10express静态文件中间件/4.redirect.js
@@ -7,6 +7,9 @@ var app = express();
 app.listen(8080);
 
 app.get('/', function (req, res) {
-    res.redirect('http://www.baidu.com');
+    // res.redirect('http://www.baidu.com')
+    res.setHeader('Location', 'http://www.baidu.com');
+    res.statusCode = 302;
+    res.end();
 });
 //# sourceMappingURL=4.redirect.js.map
--- node-day5/The-fourth-chapter/课件10express静态文件中间件/4.redirect.js
+++ node-day5/The-fourth-chapter/课件10express静态文件中间件/4.redirect.js
@@ -6,6 +6,8 @@ let app = express()
 app.listen(8080)
 
 app.get('/',function (req,res) {
-    res.redirect('http://www.baidu.com')
-    
+    // res.redirect('http://www.baidu.com')
+    res.setHeader('Location','http://www.baidu.com')
+    res.statusCode =302
+    res.end()
 })
